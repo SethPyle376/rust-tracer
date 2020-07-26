5943b41da36be7bd24f4292991c1d507150b3fce
Seth Pyle <seth.pyle86@gmail.com>
2020-07-26 17:00:32 -0600

    implemented canvas save

--- src/rust_tracer/canvas.rs
+++ src/rust_tracer/canvas.rs
@@ -1,5 +1,8 @@
-use crate::rust_tracer::math::color::Color;
 use std::clone::Clone;
+use std::fs::File;
+use std::string::ToString;
+use crate::rust_tracer::math::color::Color;
+use std::io::Write;
 
 pub struct Canvas {
     pub width: usize,
@@ -21,5 +24,27 @@ impl Canvas {
 
     pub fn pixel_at(&self, x: usize, y: usize) -> &Color {
         &(self.pixels[x][y])
+    }
+
+    pub fn save(&self, filename: String) -> std::io::Result<()> {
+        let mut file = File::create(filename)?;
+        file.write(b"P3\n")?;
+        file.write(format!("{} {}\n", self.width, self.height).as_bytes())?;
+        file.write(b"255\n");
+
+        for i in self.pixels.iter().flatten() {
+            let r = i.r / 1.0 * 255.0;
+            let r = r as i64;
+
+            let g = i.g / 1.0 * 255.0;
+            let g = g as i64;
+
+            let b = i.b / 1.0 * 255.0;
+            let b = b as i64;
+
+            file.write(format!("{} {} {}\n", r, g, b).as_bytes());
+        }
+
+        Ok(())
     }
 }
--- src/tests/canvas.rs
+++ src/tests/canvas.rs
@@ -13,3 +13,15 @@ fn canvas_pixel_test() {
     assert_eq!(color2.g, 1.0);
     assert_eq!(color2.b, 1.0);
 }
+
+#[test]
+#[ignore]
+fn canvas_save_test() {
+    let mut canvas = Canvas::new(1280, 720);
+    let color1 = Color::new(1.0, 1.0, 1.0);
+
+    canvas.write_pixel(1279, 719, color1);
+    let color2 = canvas.pixel_at(1279, 719);
+
+    canvas.save("test.ppm".to_string());
+}
