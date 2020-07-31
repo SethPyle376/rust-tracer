4e774a26bfd74c128e35baae679d3370d0a63b48
Seth Pyle <seth.pyle86@gmail.com>
2020-07-31 08:03:20 -0600

    first raytraced image implemented

--- src/tests/canvas.rs
+++ src/tests/canvas.rs
@@ -1,5 +1,9 @@
 use crate::rust_tracer::canvas::Canvas;
 use crate::rust_tracer::math::color::Color;
+use crate::rust_tracer::math::sphere::Sphere;
+use crate::rust_tracer::math::mat4::Mat4;
+use crate::rust_tracer::math::point::Point;
+use crate::rust_tracer::math::ray::Ray;
 
 #[test]
 fn canvas_pixel_test() {
@@ -24,4 +28,44 @@ fn canvas_save_test() {
     let color2 = canvas.pixel_at(1279, 719);
 
     canvas.save("test.ppm".to_string());
+}
+
+#[test]
+#[ignore]
+fn basic_ray_tracing_test() {
+    let canvas_dimension = 100;
+    let wall_size = 7.0;
+    let mut canvas = Canvas::new(canvas_dimension, canvas_dimension);
+
+    let pixel_size = wall_size / canvas_dimension as f32;
+    let half = wall_size / 2.0;
+
+    let sphere = Sphere::new(Mat4::identity(), 0);
+
+
+    for y in 0..(canvas_dimension - 1) {
+        let world_y = half - pixel_size * y as f32;
+        for x in 0..(canvas_dimension - 1) {
+            let world_x = -half + pixel_size * x as f32;
+
+            let position = Point::new(world_x, world_y, 10.0);
+
+            let mut direction = &position - &Point::new(0.0, 0.0, -5.0);
+            direction.normalize();
+
+            let ray = Ray::new(Point::new(0.0, 0.0, -5.0), direction);
+
+            let intersection = sphere.intersect(&ray);
+
+            let mut color = Color::new(0.0, 0.0, 0.0);
+
+            if !intersection.is_empty() {
+                color.r = 1.0;
+            }
+
+            canvas.write_pixel(x, y, color);
+        }
+    }
+
+    canvas.save("basic_sphere_test.ppm".to_string());
 }
